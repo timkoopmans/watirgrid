dff9bb8552015ba778e9a6bc4c6f74a612a1fe00
Tim Koopmans <tim.koops@gmail.com>
2010-02-28 11:52:08 +1100

    removed test file

--- test_safari.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'safariwatir'
-
-@browser = Watir::Safari.new
-
-@browser.goto("http://google.com.au/")
-@browser.text_field(:name, 'q').set("performance & test automation specialists")
-sleep 1
-@browser.button(:name, 'btnI').click
