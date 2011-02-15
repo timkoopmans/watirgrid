8ed3f549183e600e5133a0eae8aeea49d63d6e35
Tim Koopmans <tim.koops@gmail.com>
2011-02-16 10:02:11 +1100

    Fixed up grid_spec

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -15,36 +15,31 @@ describe 'WatirGrid' do
   end
 
   it 'should return how many grid are available in the tuplespace' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:read_all => true)
     grid.size.should == 5
   end
 
   it 'should read any 2 grid in the tuplespace' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:quantity => 2, :read_all => true)
     grid.size.should == 2
   end
 
   it 'should take any 1 browser in the tuplespace' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:quantity => 1, :take_all => true)
     grid.size.should == 1 
   end
 
   it 'should take all grid remaining in tuplespace' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:take_all => true)
     grid.size.should == 4
   end
 
   it 'should find no more grid in the tuplespace' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:read_all => true)
     grid.size.should == 0
   end
@@ -58,40 +53,35 @@ describe 'WatirGrid' do
   end
 
   it 'should take any 1 browser based on browser type' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:quantity => 1,
       :take_all => true, :browser_type => 'safari')
     grid.size.should == 1
   end
 
   it 'should fail to find any grid based on a specific browser type' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:quantity => 1,
       :take_all => true, :browser_type => 'firefox')
     grid.size.should == 0
   end
 
   it 'should fail to find any grid based on a unknown browser type' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:quantity => 1,
       :take_all => true, :browser_type => 'penguin')
     grid.size.should == 0
   end
 
   it 'should take any 1 browser based on specific architecture type' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:quantity => 1, 
       :take_all => true, :architecture => Config::CONFIG['arch'])
     grid.size.should == 1
   end
 
   it 'should fail to find any grid based on unknown architecture type' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:quantity => 1, 
       :take_all => true, :architecture => 'geos-1992')
     grid.size.should == 0
@@ -99,18 +89,16 @@ describe 'WatirGrid' do
 
   it 'should take any 1 browser based on specific hostname' do
     hostname = `hostname`.strip
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:quantity => 1,
       :take_all => true, 
-      :hostnames => { hostname => "127.0.0.1"}
+      :hostnames => { hostname => '127.0.0.1'}
       )
     grid.size.should == 1
   end
 
   it 'should fail to find any grid based on unknown hostname' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:quantity => 1,
       :take_all => true, :hostnames => { 
         "tokyo" => "127.0.0.1"})
@@ -118,8 +106,7 @@ describe 'WatirGrid' do
   end
   
   it 'should take the last browser and execute some watir commands' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:quantity => 1, :take_all => true)
     threads = []
     grid.browsers.each do |browser|
@@ -131,6 +118,7 @@ describe 'WatirGrid' do
         b.goto("http://www.google.com")
         b.text_field(:name, 'q').set("watirgrid")
         b.button(:name, "btnI").click
+        b.close
       end
     end
     threads.each {|thread| thread.join}
@@ -138,8 +126,7 @@ describe 'WatirGrid' do
   end
 
   it 'should find no more tuples in the tuplespace' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:read_all => true)
     grid.size.should == 0
   end
