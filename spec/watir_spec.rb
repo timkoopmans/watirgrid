f513db6289c91555813fa745b204d3c3c3484167
Tim Koopmans <tim.koops@gmail.com>
2011-02-16 10:11:28 +1100

    Fixed watir_spec

--- spec/watir_spec.rb
+++ spec/watir_spec.rb
@@ -4,7 +4,6 @@ describe 'WatirGrid' do
   before(:all) do
     controller = Controller.new(
       :ring_server_port => 12351,
-      :ring_server_host => '127.0.0.1',
       :loglevel => Logger::ERROR)
     controller.start
     1.upto(1) do 
@@ -16,22 +15,19 @@ describe 'WatirGrid' do
   end
 
   it 'should return how many grid are available in the tuplespace' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:read_all => true)
     grid.size.should == 1
   end
 
   it 'should find at least one browser in the tuplespace' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:quantity => 1, :read_all => true)
     grid.size.should == 1
   end
  
   it 'should take the first (and last) browser and execute some watir commands' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:quantity => 1, :take_all => true)
     threads = []
     grid.browsers.each do |browser|
@@ -43,6 +39,7 @@ describe 'WatirGrid' do
         b.goto("http://www.google.com")
         b.text_field(:name, 'q').set("watirgrid")
         b.button(:name, "btnI").click
+        b.close
       end
     end
     threads.each {|thread| thread.join}
@@ -50,11 +47,8 @@ describe 'WatirGrid' do
   end
 
   it 'should find no more tuples in the tuplespace' do
-    grid = Watir::Grid.new(:ring_server_port => 12351, 
-    :ring_server_host => '127.0.0.1')
+    grid = Watir::Grid.new(:ring_server_port => 12351)
     grid.start(:read_all => true)
     grid.size.should == 0
   end
-
 end
-
